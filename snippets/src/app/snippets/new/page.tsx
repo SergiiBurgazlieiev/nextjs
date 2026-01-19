0d2f0bbbcbaaa0a815489221b9d30201373cdd71
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2026-01-19 11:31:39 -0600

    implement create snippet simple server action

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,6 +1,29 @@
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
+
 const SnippetCreatePage = () => {
+	const createSnippet = async (formData: FormData) => {
+		// 1. This is needs to be a server action
+		'use server'; // this defines server action nextjs is going to treat this as server action
+
+		// 2. User entered a. valid input
+		const title = formData.get('title') as string;
+		const code = formData.get('code') as string;
+
+		// 3. Create a new record in the BD
+		await db.snippet.create({
+			data: {
+				title,
+				code,
+			},
+		});
+
+		// 4. Take user back to the Home Page
+		redirect('/');
+	};
+
 	return (
-		<form>
+		<form action={createSnippet}>
 			<h3 className='font-bold m-3'>Create a Snippet</h3>
 			<div className='flex flex-col gap-4'>
 				<div className='flex gap-4'>

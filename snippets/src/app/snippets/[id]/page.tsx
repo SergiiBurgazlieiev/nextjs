452511b486d3e6ceb739629a6e18725f0c2b6e67
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2026-01-19 12:10:38 -0600

    create dynamic route and fetch snippet by its ID number

---
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+
+interface SnippetShowPageProps {
+	params: Promise<{ id: string }>;
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+	const { id } = await props.params;
+	const snippet = await db.snippet.findFirst({
+		where: { id: parseInt(id) },
+	});
+
+	// If snippet was not found return early and call notFound
+	if (!snippet) {
+		return notFound();
+	}
+
+	return <div>{snippet.title}</div>;
+}

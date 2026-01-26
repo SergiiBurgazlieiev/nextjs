514956466db59521b24066d11a90e381533cf38a
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2026-01-26 11:44:45 -0600

    fetch snippet from DB and add notFout functionality

--- snippets/src/app/snippets/[id]/edit/page.tsx
+++ snippets/src/app/snippets/[id]/edit/page.tsx
@@ -1,8 +1,19 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+
 interface SnippetEditPageProps {
 	params: Promise<{ id: string }>;
 }
 
 export default async function SnippetEditPage(props: SnippetEditPageProps) {
 	const { id } = await props.params;
-	return <div>Editing snippet with id {parseInt(id)}</div>;
+	const snippet = await db.snippet.findFirst({
+		where: { id: parseInt(id) },
+	});
+
+	if (!snippet) {
+		return notFound();
+	}
+
+	return <div>Editing snippet with title {snippet.title}</div>;
 }

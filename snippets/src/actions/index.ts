b608ea98abe7c4cd06bbe3706dc18a2f786e4fe4
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2026-01-27 11:55:22 -0600

    display form validation error message to the user from serverAction - use useActionState and startTransition

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -19,3 +19,35 @@ export async function deleteSnippet(id: number) {
 
 	redirect('/');
 }
+
+export const createSnippet = async (
+	formState: { message: string },
+	formData: FormData,
+) => {
+	// 1. User entered a. valid input
+	const title = formData.get('title');
+	const code = formData.get('code');
+
+	if (typeof title !== 'string' || title.length < 3) {
+		return {
+			message: 'Title must be longer!',
+		};
+	}
+
+	if (typeof code !== 'string' || code.length < 10) {
+		return {
+			message: 'Code must be longer!',
+		};
+	}
+
+	// 2. Create a new record in the BD
+	await db.snippet.create({
+		data: {
+			title,
+			code,
+		},
+	});
+
+	// 3. Take user back to the Home Page
+	redirect('/');
+};
--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,29 +1,25 @@
-import { db } from '@/db';
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useActionState, startTransition } from 'react';
+import { createSnippet } from '@/actions';
 
 const SnippetCreatePage = () => {
-	const createSnippet = async (formData: FormData) => {
-		// 1. This is needs to be a server action
-		'use server'; // this defines server action nextjs is going to treat this as server action
+	const [formState, action] = useActionState(createSnippet, { message: '' });
 
-		// 2. User entered a. valid input
-		const title = formData.get('title') as string;
-		const code = formData.get('code') as string;
-
-		// 3. Create a new record in the BD
-		await db.snippet.create({
-			data: {
-				title,
-				code,
-			},
+	/**
+	 * If we want to opt-out of automatic form reset,
+	 * we should continue using onSubmit like so:
+	 */
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		startTransition(() => {
+			action(formData);
 		});
-
-		// 4. Take user back to the Home Page
-		redirect('/');
 	};
 
 	return (
-		<form action={createSnippet}>
+		<form onSubmit={handleSubmit}>
 			<h3 className='font-bold m-3'>Create a Snippet</h3>
 			<div className='flex flex-col gap-4'>
 				<div className='flex gap-4'>
@@ -46,6 +42,11 @@ const SnippetCreatePage = () => {
 						id='code'
 					/>
 				</div>
+				{formState.message && (
+					<div className='my-2 p-2 bg-red-200 border rounded border-red-400'>
+						{formState.message}
+					</div>
+				)}
 				<button type='submit' className='rounded p-2 bg-blue-300'>
 					Create Snippet
 				</button>

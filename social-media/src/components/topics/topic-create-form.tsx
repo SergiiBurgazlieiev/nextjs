6d88c9437c8cafed9a51836dc2aa07ad56fa9524
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2026-01-31 13:41:08 -0600

    add create a topic form and wireup action

--- social-media/src/components/topics/topic-create-form.tsx
+++ social-media/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,35 @@
+import {
+	Input,
+	Button,
+	Textarea,
+	Popover,
+	PopoverTrigger,
+	PopoverContent,
+} from '@nextui-org/react';
+import { createTopic } from '@/actions';
+
 export default function TopicCreateForm() {
-	return <div>Topic Create Form</div>;
+	return (
+		<Popover placement='left'>
+			<PopoverTrigger>
+				<Button color='primary'>Create a Topic</Button>
+			</PopoverTrigger>
+			<PopoverContent>
+				<form action={createTopic}>
+					<div className='flex flex-col gap-4 p-4 w-80'>
+						<h3 className='text-lg'>Create a Topic </h3>
+						<Input label='Name' labelPlacement='outside' placeholder='Name' />
+						<Textarea
+							label='Description'
+							labelPlacement='outside'
+							placeholder='Description'
+						/>
+						<Button type='submit' color='primary'>
+							Create Topic
+						</Button>
+					</div>
+				</form>
+			</PopoverContent>
+		</Popover>
+	);
 }

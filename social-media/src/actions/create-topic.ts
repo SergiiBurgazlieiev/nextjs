9421e0d86f60e2a1b7fe5c3ba46e2df115245190
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2026-01-31 14:49:20 -0600

    create topic form validation and save topic to the db

--- social-media/src/actions/create-topic.ts
+++ social-media/src/actions/create-topic.ts
@@ -1,5 +1,85 @@
 'use server';
 
-export async function createTopic() {
-	// TODO: revalidate the homepage
+import { z } from 'zod';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+import paths from '@/paths';
+import { db } from '@/db';
+import type { Topic } from '@prisma/client';
+
+const createTopicSchema = z.object({
+	name: z
+		.string()
+		.min(3)
+		.regex(/^[a-z-]+$/, {
+			message: 'Must be lower case letters or dashes without spaces',
+		}),
+	description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+	errors: {
+		name?: string[];
+		description?: string[];
+		_form?: string[];
+	};
+}
+
+export async function createTopic(
+	formState: CreateTopicFormState,
+	formData: FormData,
+): Promise<CreateTopicFormState> {
+	// Validate form fields
+	const result = createTopicSchema.safeParse({
+		name: formData.get('name'),
+		description: formData.get('description'),
+	});
+
+	if (!result.success) {
+		return {
+			errors: result.error.flatten().fieldErrors,
+		};
+	}
+
+	// General form errors
+	const session = await auth();
+	if (!session || !session.user) {
+		return {
+			errors: {
+				_form: ['You must be signed in to create a topic!'],
+			},
+		};
+	}
+
+	let topic: Topic;
+
+	try {
+		// Save topic to the DB
+		topic = await db.topic.create({
+			data: {
+				slug: result.data.name,
+				description: result.data.description,
+			},
+		});
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			return {
+				errors: {
+					_form: [error.message],
+				},
+			};
+		} else {
+			return {
+				errors: {
+					_form: ['Something went wrong!'],
+				},
+			};
+		}
+	}
+
+	// revalidate home page
+	revalidatePath('/');
+	// redirect to the topic show page
+	redirect(paths.topicShow(topic.slug));
 }
--- social-media/src/components/topics/topic-create-form.tsx
+++ social-media/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
 	Input,
 	Button,
@@ -5,25 +7,50 @@ import {
 	Popover,
 	PopoverTrigger,
 	PopoverContent,
+	Alert,
 } from '@nextui-org/react';
+import { useActionState, startTransition } from 'react';
 import { createTopic } from '@/actions';
 
 export default function TopicCreateForm() {
+	const [formState, action] = useActionState(createTopic, {
+		errors: {},
+	});
+
+	const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		startTransition(() => action(formData));
+	};
+
 	return (
 		<Popover placement='left'>
 			<PopoverTrigger>
 				<Button color='primary'>Create a Topic</Button>
 			</PopoverTrigger>
 			<PopoverContent>
-				<form action={createTopic}>
+				<form onSubmit={handleSubmit} noValidate>
 					<div className='flex flex-col gap-4 p-4 w-80'>
 						<h3 className='text-lg'>Create a Topic</h3>
-						<Input label='Name' labelPlacement='outside' placeholder='Name' />
+						<Input
+							name='name'
+							label='Name'
+							labelPlacement='outside'
+							placeholder='Name'
+							isInvalid={!!formState.errors.name}
+							errorMessage={formState.errors.name?.join(', ')}
+						/>
 						<Textarea
+							name='description'
 							label='Description'
 							labelPlacement='outside'
 							placeholder='Description'
+							isInvalid={!!formState.errors.description}
+							errorMessage={formState.errors.description?.join(', ')}
 						/>
+						{formState.errors._form && (
+							<Alert color='danger'>{formState.errors._form?.join(', ')}</Alert>
+						)}
 						<Button type='submit' color='primary'>
 							Create Topic
 						</Button>
